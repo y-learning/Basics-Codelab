d9dc0e1a59a61aab2ca831771df050a1026762bc
Yahya <yahyatinani@gmail.com>
2021-01-08 13:13:29 +0100

    Add a Surface composable and padding to the text

--- android/src/main/java/com/why/composetemplate/MainActivity.kt
+++ android/src/main/java/com/why/composetemplate/MainActivity.kt
@@ -2,14 +2,21 @@ package com.why.composetemplate
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = Color.Cyan) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true, name = "Preview Greeting")
